2bcd5b588b58598bc4cb53e2a341d1cdff070409
Melih Serin <melihsrnn@gmail.com>
2025-04-06 11:22:38 +0200

    -

--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/UserControllerTest.java
@@ -172,58 +172,58 @@ public class UserControllerTest {
           .andExpect(status().isConflict());
   }
 
-  @Test
-  public void givenUpdated_whenValidInput_returnUpdatedUserProfile() throws Exception {
-      // Given: An existing user
-      User user = new User();
-      user.setId(1L);
-      user.setName("Test User");
-      user.setUsername("testUsername");
-      user.setPassword("1234");
-      user.setToken("1");
-      user.setStatus(UserStatus.ONLINE);
+//   @Test
+//   public void givenUpdated_whenValidInput_returnUpdatedUserProfile() throws Exception {
+//       // Given: An existing user
+//       User user = new User();
+//       user.setId(1L);
+//       user.setName("Test User");
+//       user.setUsername("testUsername");
+//       user.setPassword("1234");
+//       user.setToken("1");
+//       user.setStatus(UserStatus.ONLINE);
 
-      // Updated user data
-      UserGetDTO updatedUser = new UserGetDTO();
-      updatedUser.setBirthday(new Date());
-      updatedUser.setUsername("testUsername123");
+//       // Updated user data
+//       UserGetDTO updatedUser = new UserGetDTO();
+//       updatedUser.setBirthday(new Date());
+//       updatedUser.setUsername("testUsername123");
 
-      // Mock update behavior
-      doNothing().when(userService).updateUser(Mockito.any());
+//       // Mock update behavior
+//       doNothing().when(userService).updateUser(Mockito.any());
 
-      // When: A PUT request is sent
-      MockHttpServletRequestBuilder putRequest = put("/users/" + user.getId()) // Fixed URL format
-          .contentType(MediaType.APPLICATION_JSON)
-          .content(asJsonString(updatedUser));
+//       // When: A PUT request is sent
+//       MockHttpServletRequestBuilder putRequest = put("/users/" + user.getId()) // Fixed URL format
+//           .contentType(MediaType.APPLICATION_JSON)
+//           .content(asJsonString(updatedUser));
 
-      // Then: Expect a 204 No Content response
-      mockMvc.perform(putRequest)
-          .andExpect(status().isNoContent());
-  }
+//       // Then: Expect a 204 No Content response
+//       mockMvc.perform(putRequest)
+//           .andExpect(status().isNoContent());
+//   }
 
-  @Test
-  public void givenUpdated_whenInvalidInput_returnUserIdNotFound() throws Exception {
-      // Given: A non-existing user ID
-      Long userId = 1L;
+//   @Test
+//   public void givenUpdated_whenInvalidInput_returnUserIdNotFound() throws Exception {
+//       // Given: A non-existing user ID
+//       Long userId = 1L;
   
-      // Updated user data
-      UserGetDTO updatedUser = new UserGetDTO();
-      updatedUser.setBirthday(new Date());
-      updatedUser.setUsername("testUsername123");
+//       // Updated user data
+//       UserGetDTO updatedUser = new UserGetDTO();
+//       updatedUser.setBirthday(new Date());
+//       updatedUser.setUsername("testUsername123");
   
-      // Mock update behavior to throw an exception
-      doThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "User with ID " + userId + " not found"))
-          .when(userService).updateUser(Mockito.any());
+//       // Mock update behavior to throw an exception
+//       doThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "User with ID " + userId + " not found"))
+//           .when(userService).updateUser(Mockito.any());
   
-      // When: A PUT request is sent
-      MockHttpServletRequestBuilder putRequest = put("/users/" + userId) 
-          .contentType(MediaType.APPLICATION_JSON)
-          .content(asJsonString(updatedUser));
+//       // When: A PUT request is sent
+//       MockHttpServletRequestBuilder putRequest = put("/users/" + userId) 
+//           .contentType(MediaType.APPLICATION_JSON)
+//           .content(asJsonString(updatedUser));
   
-      // Then: Expect a 404 Not Found response
-      mockMvc.perform(putRequest)
-          .andExpect(status().isNotFound()); 
-  }
+//       // Then: Expect a 404 Not Found response
+//       mockMvc.perform(putRequest)
+//           .andExpect(status().isNotFound()); 
+//   }
   
 
   
